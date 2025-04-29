4e8c5eb4cb104c6538e0ddaa44ed3a8d8784db77
sethk3 <threegartz@gmail.com>
2025-04-28 18:35:02 -0600

    adds landing page

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1o4RfiiSUHuGlQLHfRHH2qZa4GR0Wk2GN298nT6bGx8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zhSdQ+4sGk+auD+Zh/6s2buUupvq1SFkYoOSIDr7x+A=\"\n}"
--- .next/static/webpack/webpack.def310894b74c87d.hot-update.js
+++ .next/static/webpack/webpack.def310894b74c87d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "99d45d72b4824581"; }
+/******/ 	__webpack_require__.h = function() { return "276a7be584b4f386"; }
 /******/ }();
 /******/ 
 /******/ }
